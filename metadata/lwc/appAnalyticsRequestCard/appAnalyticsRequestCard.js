646359d146fe730259d04ba931962e8d62dbbeb9
jhaydraude <jhaydraude@salesforce.com>
2020-10-28 14:53:32 -0400

    Setup ADK Demo data

--- metadata/lwc/appAnalyticsRequestCard/appAnalyticsRequestCard.js
+++
@@ -1,5 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class AppAnalyticsRequestCard extends LightningElement {
-  @api request;
-}
--- orgInit.sh
+++ orgInit.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-
-sfdx force:org:create -f config/project-scratch-def.json -a AppAnalytics  --setdefaultusername -d 1
+sfdx force:org:create -f config/project-scratch-def.json  --setdefaultusername -d 1 --apiversion=50.0
 
 #Install LMA Package
 sfdx force:package:install -p 04t30000001DWL0 -w 20
 
+#Install App Analytics Package v 1.6
+sfdx force:package:install -p 04t3i000002KSAZ -w 20
+
+#Insert Sample Data
+sfdx force:data:tree:import -p demoAssets/data/plan.json
+
 #Push Source
-sfdx force:source:push -f
+#sfdx force:source:push -f
 
 #Grant Permission
 sfdx force:user:permset:assign -n AppAnalytics
 
 #Open Org
-sfdx force:org:open -p lightning/n/App_Analytics_Query_Requests
+sfdx force:org:open -p lightning/n/AppAnalytics__App_Analytics_Query_Requests
